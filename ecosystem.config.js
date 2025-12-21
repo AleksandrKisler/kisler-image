500d7bb9b24fd6ad69a5906c7bcac007b312ea29
root <root@kdfidjanne.local>
2025-12-21 03:21:20 +0000

    st-4

---
+++ ecosystem.config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  apps: [
+    {
+      name: 'server',
+      script: 'src/server.js',
+      instances: 1,
+      exec_mode: 'fork',
+      env: {
+        NODE_ENV: 'production'
+      },
+      error_file: 'logs/server-error.log',
+      out_file: 'logs/server-out.log',
+      log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
+      merge_logs: true,
+      autorestart: true,
+      max_memory_restart: '500M',
+      watch: false,
+      ignore_watch: ['node_modules', 'logs']
+    },
+    {
+      name: 'worker',
+      script: 'src/modules/jobs/worker.js',
+      instances: 1,
+      exec_mode: 'fork',
+      env: {
+        NODE_ENV: 'production'
+      },
+      error_file: 'logs/worker-error.log',
+      out_file: 'logs/worker-out.log',
+      log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
+      merge_logs: true,
+      autorestart: true,
+      max_memory_restart: '300M',
+      watch: false,
+      ignore_watch: ['node_modules', 'logs']
+    }
+  ],
+}
