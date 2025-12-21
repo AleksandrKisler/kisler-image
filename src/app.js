d1fd3020418df7779bb17a583e638e08670a4c93
AleksandrKisler <alexander.kisler@prospace.tech>
2025-12-21 06:21:59 +0300

    t-5

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const YAML = require("yamljs");
 const cors = require('cors');
 
 const corsOptions = {
-    origin: origin: process.env.ALLOWED_ORIGINS?.split(','),
+    origin: process.env.ALLOWED_ORIGINS?.split(','),
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
